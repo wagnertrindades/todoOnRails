e2fa302c65b1d8ca36152cc11b631762c769c2c0
Wagner Trindade <wtrindades@hotmail.com>
2015-11-27 12:40:29 -0200

    Genarate and associate todo_item model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151127143249) do
+ActiveRecord::Schema.define(version: 20151127143811) do
+
+  create_table "todo_items", force: :cascade do |t|
+    t.string   "content"
+    t.integer  "todo_list_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  add_index "todo_items", ["todo_list_id"], name: "index_todo_items_on_todo_list_id"
 
   create_table "todo_lists", force: :cascade do |t|
     t.string   "title"
