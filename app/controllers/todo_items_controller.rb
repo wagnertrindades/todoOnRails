a248762f5e8d41d74c7db61c7225df932917b1ca
Wagner Trindade <wtrindades@hotmail.com>
2015-11-27 13:21:19 -0200

    Added Create and Destroy methods in Todo Items

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,2 +1,30 @@
 class TodoItemsController < ApplicationController
+    before_action :set_todo_list
+
+    def create
+        @todo_item = @todo_list.todo_items.create(todo_item_params)
+        redirect_to @todo_list
+    end
+
+    def destroy
+        @todo_item = @todo_list.todo_items.find(params[:id])
+        
+        if @todo_item.destroy
+            flash[:success] = "O item foi deletado com sucesso."
+        else
+            flash[:error] = "O item não pode ser deletado."
+        end
+        
+        redirect_to @todo_list
+    end
+
+    private
+
+    def set_todo_list
+        @todo_list = TodoList.find(params[:todo_list_id])
+    end
+
+    def todo_item_params
+        params[:todo_item].permit(:content)
+    end
 end
