75fc3e546b9562390fd6643d2dff0e471a475b71
Wagner Trindade <wtrindades@hotmail.com>
2015-11-30 20:01:02 -0200

    Config environments

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20151127152543) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "todo_items", force: :cascade do |t|
     t.string   "content"
     t.integer  "todo_list_id"
@@ -21,7 +24,7 @@ ActiveRecord::Schema.define(version: 20151127152543) do
     t.datetime "completed_at"
   end
 
-  add_index "todo_items", ["todo_list_id"], name: "index_todo_items_on_todo_list_id"
+  add_index "todo_items", ["todo_list_id"], name: "index_todo_items_on_todo_list_id", using: :btree
 
   create_table "todo_lists", force: :cascade do |t|
     t.string   "title"
@@ -30,4 +33,5 @@ ActiveRecord::Schema.define(version: 20151127152543) do
     t.datetime "updated_at",  null: false
   end
 
+  add_foreign_key "todo_items", "todo_lists"
 end
